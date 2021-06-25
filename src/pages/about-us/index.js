478bd476d96c399399df727c9d78653ca938fda6
DanielMaina <daniel_maina9@hotmail.com>
2021-06-24 23:44:35 -0700

    UI: need to put privacy policy on site

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -6,11 +6,13 @@ import promoVideo from "../../assets/movies/rover-e-Bike-short.mp4";
 import image1 from "../../assets/images/about-us/image1.png";
 import image2 from "../../assets/images/about-us/image2.png";
 
+import FeaturedArticles from "../../components/FeaturedArticles";
+
 class AboutUsPage extends React.Component {
   constructor() {
     super();
 
-    setTitle("Rover E-Bike | ", true);
+    setTitle("Rover E-Bike | About Us", true);
 
     this.state = {};
   }
@@ -23,19 +25,22 @@ class AboutUsPage extends React.Component {
         </div>
 
         <div className="row">
-          <div className="col col-image">
-            <img src={image1} />
-          </div>
+          <div
+            className="col col-image"
+            style={{ backgroundImage: "url(" + image1 + ")" }}
+          ></div>
           <div className="col col-text">
-            <h4>A unique experience</h4>
-            <p>
-              We believe in raising the standard of torque sensor system and
-              promoting them.
-            </p>
-            <p>
-              We keep on working to strengthen e-bike standard in the market
-              with our unique collections.
-            </p>
+            <div>
+              <h4>A unique experience</h4>
+              <p>
+                We believe in raising the standard of torque sensor system and
+                promoting them.
+              </p>
+              <p>
+                We keep on working to strengthen e-bike standard in the market
+                with our unique collections.
+              </p>
+            </div>
           </div>
         </div>
 
@@ -49,17 +54,22 @@ class AboutUsPage extends React.Component {
 
         <div className="row">
           <div className="col col-text">
-            <h4>Designed to impress</h4>
-            <p>
-              We take riding fashion trends very seriously. We are a team of
-              riding enthusiasts who love riding from the depth of our hearts.
-            </p>
-            <p>It is one of the reasons for us to launch a brand for e-bike.</p>
+            <div>
+              <h4>Designed to impress</h4>
+              <p>
+                We take riding fashion trends very seriously. We are a team of
+                riding enthusiasts who love riding from the depth of our hearts.
+              </p>
+              <p>
+                It is one of the reasons for us to launch a brand for e-bike.
+              </p>
+            </div>
           </div>
           <div className="col col-image">
-            <div className="col col-image">
-              <img src={image2} />
-            </div>
+            <div
+              className="col col-image"
+              style={{ backgroundImage: "url(" + image2 + ")" }}
+            ></div>
           </div>
         </div>
 
@@ -71,7 +81,37 @@ class AboutUsPage extends React.Component {
             torque sensor system will make long distance or uphill easily
             without any pain.
           </p>
+          <a
+            href="/product"
+            class="button-dark"
+            style={{ float: "right", marginTop: "20px" }}
+          >
+            Explore our bikes
+          </a>
         </div>
+
+        <FeaturedArticles
+          items={[
+            {
+              title: "JOIN US ON OUR FIRST SOCIAL BIKE EVENT!",
+              content:
+                "Bring your bike and your smile. Join us on our 1st ever social 100 KM bike event. Together we will ride the world. <br/><br/>" +
+                "Snacks and coffee will be provided at the meet up place! <br/><br/>" +
+                "Date: September 1st, 2021" +
+                "Location: Stanley Park",
+            },
+            {
+              title: "Lorem ipsum dolor sid amet",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              title: "Lorem ipsum dolor sid amet",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+          ]}
+        />
       </div>
     );
   }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import "./index.css";
 import setTitle from "../../tools.js";
-import promoVideo from "../../assets/movies/rover-e-Bike-short.mp4";
+import promoVideoSource from "../../assets/movies/rover-e-Bike-short.mp4";
 import HomeProduct from "components/HomeProduct";
+import Grid from "components/Grid";
+import Reviews from "components/Reviews";
+
+import sectionMovie1 from "../../assets/movies/3d_1_torque.mp4";
+import sectionImage1 from "../../assets/images/homepage-image.png";
+import sectionMovie2 from "../../assets/movies/3d_5_battery.mp4";
+import gridBackgroundImage from "../../assets/images/home-page-grid-bg.png";
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -12,22 +19,145 @@ class HomePage extends React.Component {
     setTitle("Rover E-Bike | Home", true);
 
     this.state = {};
+    this.promoVideo = React.createRef();
+    this.sectionVideo = React.createRef();
+  }
+
+  componentDidMount() {
+    this.promoVideo.current.play();
   }
 
   render() {
     return (
       <div className="home">
         <div className="main-content">
-          <video src={promoVideo} muted loop autoPlay></video>
+          <video
+            ref={this.promoVideo}
+            src={promoVideoSource}
+            muted
+            loop
+            autoPlay
+          ></video>
+          <a href="/product" className="learn-more-button">
+            Learn More
+          </a>
         </div>
 
         <div className="offer-text">
           <p>
-            EXCLUSIVE OFFER FOR A <span>LIMITED TIME ONLY</span> - FIRST 50
-            ROVER E-BIKES ARE NOW ON SALE FOR <span>1,899 CAD!</span>
+            EXCLUSIVE OFFER FOR A LIMITED TIME ONLY - FIRST 50 ROVER E-BIKES ARE
+            NOW ON SALE FOR 1,899 CAD!
           </p>
         </div>
         <HomeProduct />
+
+        <div
+          className="section-wrapper"
+          onMouseEnter={() => {
+            this.sectionVideo.current.play();
+          }}
+        >
+          <div className="product-section-desc odd">
+            <div>
+              <h4>One of a kind torque sensor system</h4>
+              <p>Guarantees the smoothest rides under any road condition.</p>
+            </div>
+          </div>
+
+          <div className="product-section-image">
+            <video ref={this.sectionVideo} src={sectionMovie1} muted></video>
+          </div>
+        </div>
+
+        <div className="section-wrapper">
+          <div className="product-section-desc even">
+            <div>
+              <h4>SHIMANO 7-Speed Freewheel</h4>
+              <p>Ride anywhere with trustworthy Shimano TZ500 7-Speed 14-34t Freewheel.</p>
+            </div>
+          </div>
+
+          <div className="product-section-image">
+            <img src={sectionImage1} />
+          </div>
+        </div>
+
+        <div
+          className="section-wrapper"
+          onMouseEnter={() => {
+            this.sectionVideo.current.play();
+          }}
+        >
+          <div className="product-section-desc odd">
+            <div>
+              <h4>High Capacity Battery</h4>
+              <p>Detachable &amp; long lasting battery(36V, 350W) provides convienient 
+                 charging experience (fully charged within 5hrs) and allows riding up 
+                 to 160km.</p>
+            </div>
+          </div>
+
+          <div className="product-section-image">
+            <video ref={this.sectionVideo} src={sectionMovie2} muted></video>
+          </div>
+        </div>
+
+        <Grid
+          style={{
+            backgroundImage: "url(" + gridBackgroundImage + ")",
+            height: "890px",
+          }}
+          items={[
+            {
+              icon: "/img/icons/canadian-maple-leaf.svg",
+              title: "Canadian Own",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/thumbs-up.svg",
+              title: "Best Value",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/bike.svg",
+              title: "Torque Sensor",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/bike-rider.svg",
+              title: "Smoothest Ride",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/carbon_skill-level-advanced.svg",
+              title: "Upgradable",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/easy-fix.svg",
+              title: "Easy Fix",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/sun-clock.svg",
+              title: "Flexible Biking",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              icon: "/img/icons/shipping.svg",
+              title: "Shipping Ready",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+          ]}
+        />
 
         <div className="center-seperator">
           <h5>Free shipping and 7 days free return Canada wide.</h5>
@@ -35,8 +165,30 @@ class HomePage extends React.Component {
             Different rates will be applied for US. Head to UPS website for more
             an estimated delivery fee.
           </p>
-        </div> 
+        </div>
 
+        <Reviews
+          items={[
+            {
+              name: "Tess J.",
+              image: "/img/reviews/review-1.png",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              name: "Devin W.",
+              image: "/img/reviews/review-2.png",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+            {
+              name: "Paul A.",
+              image: "/img/reviews/review-3.png",
+              content:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            },
+          ]}
+        />
       </div>
     );
   }
