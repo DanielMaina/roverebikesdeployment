1b71020130e3df7ad2b86236a390f28d523cca12
Erdem <63709771+erdem-studiomawa@users.noreply.github.com>
2021-07-04 01:09:09 +0300

    Update index.js

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -12,7 +12,7 @@ import FeaturedArticles from "../../components/FeaturedArticles";
 class AboutUsPage extends React.Component {
   constructor() {
     super();
-
+    this.isMobile = window.innerWidth <= 500;
     this.state = {};
   }
   componentDidUpdate() {
